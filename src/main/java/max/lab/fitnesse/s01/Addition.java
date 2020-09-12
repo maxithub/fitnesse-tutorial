85a2851130f56afd9402a944bd6c8c9eb7b9950b
Max Yuan <max.m.yuan@gmail.com>
2020-09-12 23:00:31 +0800

    Finish video #1

--- src/main/java/max/lab/fitnesse/s01/Addition.java
+++ src/main/java/max/lab/fitnesse/s01/Addition.java
@@ -1,13 +1,16 @@
 package max.lab.fitnesse.s01;
 
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Setter
 public class Addition {
 	private int a;
 	private int b;
 	
 	public int result() {
+		log.info("Calculating a+b...");
 		return a + b;
 	}
 }
